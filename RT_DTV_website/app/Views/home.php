fbc99076a4338bce65384d6869607c07c047c3e7
candycca <42653475+candycca@users.noreply.github.com>
2024-12-27 15:08:03 +0800

    Update home.php

--- RT_DTV_website/app/Views/home.php
+++ RT_DTV_website/app/Views/home.php
@@ -4,6 +4,7 @@
         <img src="pictures/system.png" alt="system">
         <img src="pictures/tutorial_1.png" alt="tutorial">
         <img src="pictures/tutorial_2.png" alt="tutorial">
+        <img src="pictures/tutorial_3.png" alt="tutorial">
     </div> 
 <?= $this->endSection() ?>
 
